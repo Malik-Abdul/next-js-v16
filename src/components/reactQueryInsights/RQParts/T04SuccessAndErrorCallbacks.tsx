1faf3eb4cb00a54431b6338fa62d2c497c73ce0a
Abdul Ghafoor <abdul.ghafoor@dubizzlelabs.com>
2026-01-27 10:28:36 +0500

    Query cache, callback for success and errors and query onClick

--- src/components/reactQueryInsights/RQParts/T04SuccessAndErrorCallbacks.tsx
+++ src/components/reactQueryInsights/RQParts/T04SuccessAndErrorCallbacks.tsx
@@ -4,33 +4,40 @@ import { useQuery } from "@tanstack/react-query";
 import { FetchUsersTypeData } from "@/types/Users";
 import { USERS_URLS } from "@/constants/api";
 import axios from "axios";
+import { useEffect } from "react";
 
 const fetchUsers = async (): Promise<FetchUsersTypeData> => {
   return axios.get(USERS_URLS.SALARY_RANGE_51480_51500);
 };
 
-const T02QueryCache = () => {
-  const { data, error, isError, isLoading, isFetching } = useQuery({
+const T04SuccessAndErrorCallbacks = () => {
+  const onSuccess = (data: FetchUsersTypeData) => {
+    console.log("Perform side effect after data fetching: ", data);
+  };
+  const onError = (error: Error) => {
+    console.log("Perform side effect after data error: ", error);
+  };
+  const { data, error, isError, isSuccess, isLoading, isFetching } = useQuery({
     queryKey: ["users"],
     queryFn: fetchUsers,
-    // gcTime: 5 * 60 * 1000, // 5 minutes is default we can change it
-    // gcTime: 5000, // 5s
-    // staleTime: 30000, // 30s and set cache time gcTime be default 5 minutes
-    // refetchOnMount: true,
-    // refetchOnMount: false,
-    // refetchOnMount: "always",
-    // refetchOnWindowFocus: true,
-    // refetchOnWindowFocus: false,
-    // refetchOnWindowFocus: "always",
-    // refetchInterval: false,
-    refetchInterval: 2000,
-    refetchIntervalInBackground: false,
+    retry: false,
   });
+  useEffect(() => {
+    if (isSuccess && data) {
+      onSuccess(data);
+    }
+  }, [isSuccess, data]);
+
+  useEffect(() => {
+    if (isError && error) {
+      onError(error);
+    }
+  }, [isError, error]);
   console.log({ isFetching, isLoading });
-  if (isLoading) {
+  if (isLoading || isFetching) {
     return (
       <Fragment>
-        <h2>T02QueryCache</h2>
+        <h3>T04SuccessAndErrorCallbacks</h3>
         <div>Loading...</div>
       </Fragment>
     );
@@ -38,15 +45,16 @@ const T02QueryCache = () => {
   if (isError) {
     return (
       <Fragment>
-        <h2>T02QueryCache</h2>
+        <h3>T04SuccessAndErrorCallbacks</h3>
         <div>{(error as Error).message}</div>
       </Fragment>
     );
   }
   return (
     <Fragment>
-      <h2>T02QueryCache</h2>
-      <h3>Users</h3>
+      <h3>T04SuccessAndErrorCallbacks</h3>
+
+      <h4>Users</h4>
       {data &&
         data?.data.map((u) => (
           <div key={u.id}>
@@ -58,4 +66,4 @@ const T02QueryCache = () => {
   );
 };
 
-export default T02QueryCache;
+export default T04SuccessAndErrorCallbacks;
